f4c7140101353b7e9b06d1b96ab73d4671ddef67
Tong Ju <tju@uchicago.edu>
2016-11-29 23:40:16 -0600

    rearrange the file structure

--- 00_runfile.R
+++ 00_runfile.R
@@ -1,8 +1,9 @@
 
 library(feather)
 
+
 ## clean out any previous work
-paths <- c("rawdata", "data", "graph", "segdata", "output")
+paths <- c("fp-jtbeyond/data", "fp-jtbeyond/graph")
 
 for(path in paths){
   unlink(path, recursive = TRUE)    # delete folder and contents; "recursive" to further delete all the subfolder in the folder.
@@ -15,4 +16,4 @@ source("02_Chinese_segmentation.R")
 source("03_analysis_1.R")
 source("04_analysis_2.R")
 
-rmarkdown::render("07_report.Rmd", output_dir = "output") # to save the rmd file into a new folder "output" 
+#rmarkdown::render("07_report.Rmd", output_dir = "output") # to save the rmd file into a new folder "output" 
